6a08a3481e5ee02cfa7c1de0d3296fddaf59a20a
George Mamaladze <gmamaladze@gmail.com>
2019-04-13 01:11:41 +0200

    Implemented LCD display and special char

--- controller/display.py
+++ controller/display.py
@@ -1,9 +1,28 @@
 import grove_rgb_lcd as lcd
 
+
+
 class Display:
+
+    # Display symbols are only: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "L", "С", "Г", "Е", " "];
+
+    RUSSIAN_R_PATTERN = [
+        0b11111,
+        0b10000,
+        0b10000,
+        0b10000,
+        0b10000,
+        0b10000,
+        0b10000,
+        0b10000
+    ]
+
+    CUSTOM_CHAR_CODE = 0
 
     def __init__(self):
         lcd.setRGB(0, 255, 0)
+        lcd.create_char(Display.CUSTOM_CHAR_CODE, Display.RUSSIAN_R_PATTERN)
 
     def show(self, text):
-        print(text)
+        text.replace("Г", chr(Display.CUSTOM_CHAR_CODE))
+        lcd.setText(text)
